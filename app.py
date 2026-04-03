d8f440f57d7b0577c3728c81f3e69c79ddef6680
Zebex <24cs12@lingayasvidyapeeth.edu.in>
2026-04-03 12:18:24 +0530

    Chunks issue fix

--- app.py
+++ app.py
@@ -85,15 +85,35 @@ async def receive_message(request: Request):
             F.send_message(sender, "PDF recived. Processing....")
             print("PDF Processing time...", time.time() - start)
             
-            file_path = F.download_pdf(media_id)
-            loader = PyPDFLoader(file_path)
-            docs = loader.load()[:10] #only first 10 pages
+            try:
+                file_path = download_pdf(media_id)
+                loader = PyPDFLoader(file_path)
+                docs = loader.load()
 
-            splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=30)
-            chunks = splitter.split_documents(chunks, embeddings)
-            db = FAISS.from_documents(chunks, embeddings)
-            user_db[sender] = db 
-            F.send_message(sender, " PDF Processed! Now ask you Questions.")
+                if not docs:
+                    send_message(sender, "❌ PDF is empty or unreadable")
+                    return {"status": "error"}
+                
+                splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=300, 
+                    chunk_overlap=30
+                )
+
+                chunks = splitter.split_documents(docs)
+
+                if not chunks:
+                    send_message(sender, "❌ failed to process PDF ")
+                    return {"status": "error"}
+                
+                db = FAISS.from_documents(chunks, embeddings)
+                user_dbs[sender] = db
+                send_message(sender, "✅ PDF processed! Ask your question.")
+
+            except Exception as e:
+                print("PDF ERROR: ", e)
+                send_message(sender, "❌ Error processing PDF")
+
+
 
         # Case2 : User SENT Text:
         elif "text" in message_data:
